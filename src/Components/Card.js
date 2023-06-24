1f0e0718b1acb2d12124096028309655cf4eb894
shivamk012 <shivamkoolwal14@gmail.com>
2023-06-24 21:37:41 +0530

    responsivity

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,7 @@ export default function Card({data}) {
     return <div className="fixHeight" style={{"backgroundColor" : `${card.color}`}}>
         <div style={{"flex" : "4" , "padding-top" : "25%" , "padding-left" : "10%"}}> 
             <div style={{"fontSize" : "1rem"}}>{card.text}</div>
-            <div style={{"fontSize" : "1.5rem"}}>{card.value}</div>
+            <div style={{"fontSize" : "1.5rem" , "fontWeight" : "700"}}>{card.value}</div>
         </div>
         <div style={{"flex" : "1" , "padding-top" : "10%" , "fontSize" : "1.5rem" , "color" : "black"}}>{card.icon}</div>
     </div>
