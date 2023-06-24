64ed18fabaa19c06054d5a6a5f39a68de0aa7173
shivamk012 <shivamkoolwal14@gmail.com>
2023-06-24 18:50:16 +0530

    google cred change

--- src/Components/GoogleAuth.js
+++ src/Components/GoogleAuth.js
@@ -3,7 +3,7 @@ import LoginPage from '../Pages/LoginPage';
 
 export default function GoogleAuth() {
   return (
-    <GoogleOAuthProvider clientId="646436992804-tleglrtvnt8fg7lkag4kmpjqn91i7edu.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="277151666562-q2esfeec0ihh3o4b61qh4j86skm4v3tt.apps.googleusercontent.com">
       <div className="App">
         <LoginPage/>
       </div>
